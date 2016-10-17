e5ae2f877fd02768aa49e175be0332ba53561def
Nadav Dav <nadavik@gmail.com>
2016-10-17 14:20:35 +0300

    added CLI

--- index.js
+++ index.js
@@ -1,4 +1,16 @@
+#!/usr/bin/env node
 const {KatanServer} = require('./src/KatanServer');
+const program = require('commander');
 
-const server = new KatanServer();
+program
+  .version('1.0.0')
+  .option('-f, --file <filename>', 'Set the data file path')
+  .option('-p, --port <number>', 'Set port number, default is 53411')
+  .parse(process.argv);
+
+const server = new KatanServer({
+  port: program.port || 53411,
+  file: program.file || process.cwd()+'/katan-db.json'
+});
+
 server.start();
